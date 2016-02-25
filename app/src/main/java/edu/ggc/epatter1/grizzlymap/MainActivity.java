8170d61b5d2ba20e91614a51ae08e2f6839537ff
jortiz <josepho011235@gmail.com>
2016-02-24 20:30:08 -0500

    added source file

--- app/src/main/java/edu/ggc/epatter1/grizzlymap/MainActivity.java
+++ app/src/main/java/edu/ggc/epatter1/grizzlymap/MainActivity.java
@@ -1,18 +1,10 @@
 package edu.ggc.epatter1.grizzlymap;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
 //import com.google.android.gms.appindexing.Action;
 //import com.google.android.gms.appindexing.AppIndex;
@@ -35,23 +27,23 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Area_map.class));
             }
         });
-//        ImageButton campusMap = (ImageButton) findViewById(R.id.btnCampusMap);
-//        campusMap.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, Campus_map.class));
-//            }
-//        });
-//
-//        ImageButton about = (ImageButton) findViewById(R.id.btnAbout);
-//        about.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this,  About.class));
-//            }
-//        });
+        ImageButton campusMap = (ImageButton) findViewById(R.id.btnCampusMap);
+        campusMap.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Campus_map.class));
+            }
+        });
+
+        ImageButton about = (ImageButton) findViewById(R.id.btnAbout);
+        about.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,  AboutActivity.class));
+            }
+        });
 
 
 
